8ba39494297f0c75b7f37a011f13493291f483a4
Bartosz Baran <bartosz.baran192@gmail.com>
2023-07-02 17:42:52 +0200

    basic view with form for user with currencies from DB, and urls configuration

--- backend/currency/views.py
+++ backend/currency/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import FormView
 
-# Create your views here.
+from . import forms, models
+
+
+class CurrencyFormView(FormView):
+    form_class = forms.CurrencyForm
+    template_name = "currency/currency_view.html"
+    success_url = "/"
+
+    def get_form(self, form_class=None):
+        currencies = models.CurrencyName.objects.all()
+        form = super().get_form(form_class)
+
+        form.fields["currency"].choices = [(currency.code, currency.name.title()) for currency in currencies]
+
+        return form
